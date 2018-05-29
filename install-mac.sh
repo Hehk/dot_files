b39b45ba3b7c26b478a35569d65c95bd50d09a8e
Kyle Henderson <kyle1101000@gmail.com>
2018-05-29 14:35:22 -0700

    fix

--- install-mac.sh
+++ install-mac.sh
@@ -2,5 +2,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
-brew install tmux node zsh
-brew install the_silver_searcher
+brew install tmux node zsh the_silver_searcher
